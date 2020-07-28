b2c1c8a846e35307f72817e6d0a7f4356b4c2bd1
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-28 16:35:15 +0300

    простенький проект с курса юдеми - польза была в применении лямбды с методом в слушателе кнопки ... )))

--- app/src/main/java/com/omelchenkoaleks/randomselection/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/randomselection/MainActivity.kt
@@ -2,11 +2,42 @@ package com.omelchenkoaleks.randomselection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ask_me_button.setOnClickListener{shakeBall()}
+    }
+
+    private fun shakeBall() {
+        val imageResource = when (Random().nextInt(20) + 1) {
+            1 -> R.drawable.magic_8_ball_1
+            2 -> R.drawable.magic_8_ball_2
+            3 -> R.drawable.magic_8_ball_3
+            4 -> R.drawable.magic_8_ball_4
+            5 -> R.drawable.magic_8_ball_5
+            6 -> R.drawable.magic_8_ball_6
+            7 -> R.drawable.magic_8_ball_7
+            8 -> R.drawable.magic_8_ball_8
+            9 -> R.drawable.magic_8_ball_9
+            10 -> R.drawable.magic_8_ball_10
+            11 -> R.drawable.magic_8_ball_11
+            12 -> R.drawable.magic_8_ball_12
+            13 -> R.drawable.magic_8_ball_13
+            14 -> R.drawable.magic_8_ball_14
+            15 -> R.drawable.magic_8_ball_15
+            16 -> R.drawable.magic_8_ball_16
+            17 -> R.drawable.magic_8_ball_17
+            18 -> R.drawable.magic_8_ball_18
+            19 -> R.drawable.magic_8_ball_19
+            else -> R.drawable.magic_8_ball_20
+        }
+
+        ball_image_view.setImageResource(imageResource)
     }
 }
